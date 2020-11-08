9af8cb44f74f37ec9b53c53c4357b43c579cf9d8
wadood <wadoodislam@gmail.com>
2020-11-09 01:44:53 +0500

    Yolo Running

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 
 # global sources for video feeds
 web_cam = cv2.VideoCapture(0)  # web-cam feed
-mob_cam2 = cv2.VideoCapture()  # mobile cam 1
-mob_cam3 = cv2.VideoCapture()  # mobile cam 2
+# mob_cam2 = cv2.VideoCapture()  # mobile cam 1
+# mob_cam3 = cv2.VideoCapture()  # mobile cam 2
 
 
 def connect_cam(cam, source):
@@ -31,7 +31,7 @@ def hello_world():
 
 def stream_video(source, filename, res):
     dimensions = get_dims(source, res)
-    out = cv2.VideoWriter(filename, get_video_type(filename), 25, dimensions)
+    out = cv2.VideoWriter('data/'+filename, get_video_type(filename), 12, dimensions)
 
     while True:
         frame = cv2.resize(source.read()[1], dimensions)
@@ -56,29 +56,29 @@ def cam1():
 
 
 # Streaming response endpoint for camera 3
-@app.route("/cam2")
-def cam2():
-    global mob_cam2
-    return Response(stream_video(mob_cam2, 'mob_cam2.avi', 'custom'),
-                    mimetype="multipart/x-mixed-replace; boundary=frame")
+# @app.route("/cam2")
+# def cam2():
+#     global mob_cam2
+#     return Response(stream_video(mob_cam2, 'mob_cam2.avi', 'custom'),
+#                     mimetype="multipart/x-mixed-replace; boundary=frame")
+#
+#
+# # Streaming response endpoint for camera 3
+# @app.route("/cam3")
+# def cam3():
+#     global mob_cam3
+#     return Response(stream_video(mob_cam3, 'mob_cam3.avi', 'custom'),
+#                     mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-# Streaming response endpoint for camera 3
-@app.route("/cam3")
-def cam3():
-    global mob_cam3
-    return Response(stream_video(mob_cam3, 'mob_cam3.avi', 'custom'),
-                    mimetype="multipart/x-mixed-replace; boundary=frame")
-
-
-# Starting threads for trying to connect to the IP cams
-cam1_t = threading.Thread(target=connect_cam, args=(mob_cam2, 'http://172.16.3.188:8080/video'))  # Thread for camera 2
-cam1_t.daemon = True
-cam1_t.start()
-
-cam2_t = threading.Thread(target=connect_cam, args=(mob_cam3, 'http://172.16.6.67:8080/video'))  # Thread for camera 3
-cam2_t.daemon = True
-cam2_t.start()
+# # Starting threads for trying to connect to the IP cams
+# cam1_t = threading.Thread(target=connect_cam, args=(mob_cam2, 'http://172.16.3.188:8080/video'))  # Thread for camera 2
+# cam1_t.daemon = True
+# cam1_t.start()
+#
+# cam2_t = threading.Thread(target=connect_cam, args=(mob_cam3, 'http://172.16.6.67:8080/video'))  # Thread for camera 3
+# cam2_t.daemon = True
+# cam2_t.start()
 
 
 if __name__ == '__main__':
