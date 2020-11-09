13f0fd36073b4d8ff7049a5ee223dc6353b781d8
wadood <wadoodislam@gmail.com>
2020-11-09 16:42:27 +0500

    optimized imports

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cv2
 from yolo.yolo import YOLO
 
 # cam = cv2.VideoCapture(0)
-cam = cv2.VideoCapture('http://192.168.100.6:8080/video')
+cam = cv2.VideoCapture(0)
 
 fps = FPS().start()
 yolo = YOLO()
@@ -16,8 +16,9 @@ start = datetime.now()
 
 
 frames = []
-
+count = 0
 while True:
+
     t1 = time.time()
     ret, frame = cam.read()
     layerOutputs = yolo.predict(frame)
@@ -26,8 +27,10 @@ while True:
     current = datetime.now()
     frames.append(frame)
     fps.update()
-    if (current - start).total_seconds() >= 10:
-        break
+    cv2.imwrite("data/frames/frame%d.jpg" % count, frame)
+    count += 1
+    # if (current - start).total_seconds() >= 10:
+    #     break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
