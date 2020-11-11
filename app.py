ecbd6d184567236db19ef942f4a681106a278359
sohaibrabbani <sohaibmasood95@gmail.com>
2020-11-11 22:20:49 +0500

    Added GPU support for YOLO

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import threading
+from datetime import datetime
 
 import cv2
 import numpy as np
 from flask import Flask, Response, render_template
+from imutils.video import FPS
 
 from utils import get_video_type, get_dims
 from yolo.yolo import YOLO
@@ -10,13 +12,13 @@ from yolo.yolo import YOLO
 app = Flask(__name__)
 
 lock1 = threading.Lock()
-stream_output1 = [None,]
+stream_output1 = [np.zeros((5,5)),]
 
 lock2 = threading.Lock()
-stream_output2 = [None,]
+stream_output2 = [np.zeros((5,5)),]
 
 lock3 = threading.Lock()
-stream_output3 = [None,]
+stream_output3 = [np.zeros((5,5)),]
 
 # global sources for video feeds
 web_cam1 = cv2.VideoCapture()  # web-cam feed
@@ -33,19 +35,33 @@ def hello_world():
 def detect_objects(source, address, filename, res, yolo, stream_output, lock):
     dimensions = get_dims(source, res)
     out = cv2.VideoWriter('data/'+filename, get_video_type(filename), 12, dimensions)
-
+    start = datetime.now()
+    fps = FPS().start()
+    printed = False
+    threshold = 5
+    count = 0
     while True:
         while not source.isOpened():
             source.open(address)
-
         try:
             grabbed, frame = source.read()
+            # count += 1
+            # if count % threshold != 0:
+            #     continue
             frame = cv2.resize(frame, dimensions)
             frame = yolo.predict(frame)
             out.write(frame)
         except:
             frame = None
 
+        current = datetime.now()
+        fps.update()
+
+        if (current - start).total_seconds() >= 10:
+            fps.stop()
+            print(fps.fps(), fps.elapsed())
+            fps = FPS().start()
+            start = datetime.now()
         with lock:
             stream_output[0] = frame.copy() if isinstance(frame, np.ndarray) else frame
 
@@ -87,19 +103,19 @@ def cam3():
                     mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-cam1_t = threading.Thread(target=detect_objects, args=(web_cam1, 0, 'web_cam1.avi', 'custom',
-                                                       YOLO(), stream_output1, lock1))  # Thread for camera 2
-cam1_t.daemon = True
-cam1_t.start()
+# cam1_t = threading.Thread(target=detect_objects, args=(web_cam1, 0, 'web_cam1.avi', 'custom',
+#                                                        YOLO(), stream_output1, lock1))  # Thread for camera 2
+# cam1_t.daemon = True
+# cam1_t.start()
+
+#
+# cam2_t = threading.Thread(target=detect_objects, args=(mob_cam2, 'http://10.47.27.57:8080/video', 'mob_vid2.avi', 'custom',
+#                                                        YOLO(), stream_output2, lock2))  # Thread for camera 3
+# cam2_t.daemon = True
+# cam2_t.start()
 
 
-cam2_t = threading.Thread(target=detect_objects, args=(mob_cam2, 'http://10.47.27.57:8080/video', 'mob_vid2.avi', 'custom',
-                                                       YOLO(), stream_output2, lock2))  # Thread for camera 3
-cam2_t.daemon = True
-cam2_t.start()
-
-
-cam3_t = threading.Thread(target=detect_objects, args=(mob_cam3, 'http://192.168.100.16:8080/video', 'mob_vid3.avi', 'custom',
+cam3_t = threading.Thread(target=detect_objects, args=(mob_cam3, 'http://192.168.100.6:8080/video', 'mob_vid3.avi', 'custom',
                                                        YOLO(), stream_output3, lock3))  # Thread for camera 2
 cam3_t.daemon = True
 cam3_t.start()
